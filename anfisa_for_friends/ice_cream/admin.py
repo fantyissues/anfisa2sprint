7747762cde8391313546d4906c3e16fafe995012
Denis Lopin <denis.lopin.w@yandex.ru>
2023-08-31 17:58:38 +0500

    Зарегистрировать модели Topping, Wrapper, IceCream в админ-зоне приложения ice_cream

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, IceCream, Topping, Wrapper
 
 admin.site.register(Category)
+admin.site.register(IceCream)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
