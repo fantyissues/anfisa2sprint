7513da5a867cc95c1ad8bffc01a2dcf78deb769b
Denis Lopin <denis.lopin.w@yandex.ru>
2023-08-31 23:09:13 +0500

    Добавить настройки админ-зоны приложения ice_cream

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -2,7 +2,43 @@ from django.contrib import admin
 
 from .models import Category, IceCream, Topping, Wrapper
 
-admin.site.register(Category)
-admin.site.register(IceCream)
+
+class IceCreamInline(admin.TabularInline):
+    model = IceCream
+    extra = 0
+
+
+@admin.register(IceCream)
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper',
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category',
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
 admin.site.register(Topping)
 admin.site.register(Wrapper)
+admin.site.empty_value_display = 'Не задано'
