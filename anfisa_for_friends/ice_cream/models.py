484b0f69869b5cb6fa2ef94bbef80813b74d6560
Denis Lopin <denis.lopin.w@yandex.ru>
2023-08-31 17:54:28 +0500

    Добавить verbose_name и verbose_name_plural для всех моделей приложения ice_cream

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -8,14 +8,26 @@ class Category(PublishedModel):
     slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
 
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
 
 class Topping(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
 
+    class Meta:
+        verbose_name = 'топпинг'
+        verbose_name_plural = 'Топпинги'
+
 
 class Wrapper(PublishedModel):
     title = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = 'обёртка'
+        verbose_name_plural = 'Обёртки'
 
 
 class IceCream(PublishedModel):
@@ -36,3 +48,6 @@ class IceCream(PublishedModel):
     toppings = models.ManyToManyField(Topping)
     is_on_main = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = 'мороженое'
+        verbose_name_plural = 'Мороженое'
